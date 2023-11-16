21bf1f3f3f1c3f0a90faf27816c5bf96a6d0970f
Davi Candido (Note) <davivc@gmail.com>
2023-11-16 17:13:05 -0300

    Hackerrank

---
+++ hackerrank/problem-solving/lonely-integer.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    a_sum = 0
+    for i in a:
+        a_sum ^= i
+        
+    return a_sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
